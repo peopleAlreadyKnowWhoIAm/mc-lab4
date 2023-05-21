33a43b9b86a9f6fa86cf70cdf7c94f3f18d17cbb
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2023-05-21 22:26:55 +0300

    Last changes

--- src/7seg-display.c
+++ src/7seg-display.c
@@ -1,22 +1,24 @@
 #include "7seg-display.h"
 
+#define DISPLAY_DOT _BV(7)
+
 const uint8_t symbol_codes[] = {
-  0x3f, //0
-  0x06, //1
-  0x5b, //2
-  0x4f, //3
-  0x66, //4
-  0x6d, //5
-  0x7d, //6
-  0x07, //7
-  0x7f, //8
-  0x6f, //9
-  0b00000010, //a
-  0b01100000, //b
-  0b00110000, //c
-  0b01000010, //d
-  0b00110000, //e
-  0b00111000, //f
+    0x3f,        // 0
+    0x06,        // 1
+    0x5b,        // 2
+    0x4f,        // 3
+    0x66,        // 4
+    0x6d,        // 5
+    0x7d,        // 6
+    0x07,        // 7
+    0x7f,        // 8
+    0x6f,        // 9
+    0b00000010,  // a
+    0b01100000,  // b
+    0b00110000,  // c
+    0b01000010,  // d
+    0b00110000,  // e
+    0b00111000,  // f
 };
 
 static volatile uint8_t buffer_data[DISPLAY_DIGIT_WIDTH];
@@ -24,8 +26,13 @@ static volatile uint8_t buffer_data[DISPLAY_DIGIT_WIDTH];
 void _subDisplayWrite();
 
 int8_t DisplayWrite(uint8_t data[]) {
-  for(uint8_t i = 0; i < DISPLAY_DIGIT_WIDTH; i++){
-    buffer_data[i] = symbol_codes[data[i]];
+  for (uint8_t i = 0; i < DISPLAY_DIGIT_WIDTH; i++) {
+    if (i == 1 || i == 3) {
+      buffer_data[i] = symbol_codes[data[i]] | DISPLAY_DOT;
+
+    } else {
+      buffer_data[i] = symbol_codes[data[i]];
+    }
   }
   return 0;
 }
@@ -39,7 +46,7 @@ void _subDisplayWrite() {
   static uint8_t counter = 0;
   DISPLAY_DIGIT_PORT |= _BV(counter);
   counter++;
-  if(counter == DISPLAY_DIGIT_WIDTH) {
+  if (counter == DISPLAY_DIGIT_WIDTH) {
     counter = 0;
   }
   DISPLAY_DIGIT_PORT &= ~_BV(counter);
